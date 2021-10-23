431da0e4ab2f1676e3d80a2d306ddc21f9982b0b
Ifrah Noor <78050266+ShayFae@users.noreply.github.com>
2021-10-23 12:47:34 -0400

    Update for-of.js

--- for-of.js
+++ for-of.js
@@ -8,7 +8,7 @@ const amounts = [61.00, 52.25, 112.99, 5.00];
 // console.log('Order total is ', total); 
 
 //For of is good for begining to end but not if you want to do other things with it like reverse etc for would be the perfered method.
-//Instead if incrementing with let i value you can set a new varible to the length of amount then taking the total variable and adding the final value and returning it to the console
+//Instead of incrementing with let i value you can set a new varible to the length of amounts then taking the total variable and adding the final value and returning it to the console
 let total = 0;
 
 for(let amount of amounts) {
