494c438589461cff7192ae079506c71335cf53e7
ShayFae <ifrahnoor108@gmail.com>
2021-10-23 22:13:42 +0000

    GOT IT!

--- whichSchool.js
+++ whichSchool.js
@@ -1,20 +1,10 @@
+//The problem was that I was console.loging the strings when I should be returning it, the function had nothing to read since I didn't give it something to give back.(PAY ATTENTION)
 const whichSchool = function(age) {
   if(age < 13) {
-    console.log("Elementary School");
+    return "Elementary School"
   } else if(age >= 13 && age <= 18) {
-    console.log("Secondary School");
+    return "Secondary School";
   } else if (age > 18) {
-    console.log("Lighthouse Labs");
+    return "Lighthouse Labs";
   }
 };
-
-whichSchool(13);
-
-//Weird, try something else
-// const whichSchool = function (age) {
-//   for (i = 13; i < 18; i++) {
-//     (whichSchool(i)).to.be.equal("Secondary School");
-//   }
-// };
-
-// console.log(whichSchool(0));
