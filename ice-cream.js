2d567600303f78b79902e20bfaf8768737adb62e
ShayFae <ifrahnoor108@gmail.com>
2021-10-24 20:07:15 +0000

    NEW

---
+++ ice-cream.js
@@ -0,0 +1,16 @@
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours)
+iceCreamFlavours.push("root beer");
+
+
+const firstFlavour = iceCreamFlavours[0];
+const lastFlavour = iceCreamFlavours[5];
+const numberOfFlavours = iceCreamFlavours.length;
+
+
+
+
+console.log(iceCreamFlavours);
+console.log(firstFlavour);
+console.log(lastFlavour);
+console.log(numberOfFlavours);
